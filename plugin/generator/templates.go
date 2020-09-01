2f8fc4551ab5803cbafd191f3cf4a8b381be300d
Sven Dowideit <SvenDowideit@home.org.au>
2020-09-01 23:29:40 +1000

    add a hostname func
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- plugin/generator/templates.go
+++ plugin/generator/templates.go
@@ -39,18 +39,32 @@ func (g *CaddyfileGenerator) getTemplatedCaddyfile(container *types.Container, l
 		"https": func() string {
 			return "https"
 		},
+		"hostname": func(options ...interface{}) (string, error) {
+			// if there is a string param, use it.
+			if len(options) == 1 {
+				if host, isString := options[0].(string); isString {
+					return host, nil
+				}
+			}
+			// TODO: how do we deal if we have a full domain name?
+
+			// TODO: service name..
+
+			// TODO: from compose, looks like caddy-docker-proxy_maintainence_1 (remove _1?)
+			return strings.TrimPrefix(container.Names[0], "/"), nil
+		},
 	}
 
 	t, err := template.New("").Funcs(sprig.TxtFuncMap()).Funcs(funcMap).Parse(`
 {{ if index .Labels "virtual.port" }}
 *.loc.alho.st loc.alho.st {
-			import dns_api_gandi                                               
-			@arg_loc_alho_st {                                        
-					host arg.loc.alho.st                    
-			}                                                                  
-			route @arg_loc_alho_st {                                  
-					reverse_proxy {{upstreams ((index .Labels "virtual.port" | int)) }}                                   
-			}                                                                  
+			import dns_api_gandi
+			@{{hostname ((index .Labels "virtual.host"))}}_loc_alho_st {
+					host {{hostname ((index .Labels "virtual.host"))}}.loc.alho.st
+			}
+			route @{{hostname ((index .Labels "virtual.host"))}}_loc_alho_st {
+					reverse_proxy {{upstreams ((index .Labels "virtual.port" | int)) }}
+			}
 }
 {{ end }}
 `)
